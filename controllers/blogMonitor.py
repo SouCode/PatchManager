f176c09774727cc42e2e8eaf168fc56730e54049
Sou <samelsonjohn@protonmail.com>
2023-11-11 22:47:28 -0500

    adding notes

--- controllers/blogMonitor.py
+++ controllers/blogMonitor.py
@@ -1,16 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+# Function to check for new updates on the Google Chrome blog
 def find_new_update():
+    # URL of the Google Chrome releases blog
     url = 'https://chromereleases.googleblog.com/'
+
+    # Send a GET request to the URL
     response = requests.get(url)
+
+    # Check if the request was successful
     if response.ok:
+        # Get the HTML content of the page
         html = response.text
+
+        # Parse the HTML content using BeautifulSoup
         soup = BeautifulSoup(html, 'html.parser')
+
+        # Iterate over each post title on the blog
         # Assuming each post title is in an 'a' tag within an 'h2' with class 'title'
         for title in soup.find_all('h2', class_='title'):
+            # Find the 'a' tag within the title
             a_tag = title.find('a')
+
+            # Check if the 'a' tag exists and contains the specific update text
             if a_tag and "Beta Channel Update for ChromeOS / ChromeOS Flex" in a_tag.text:
+                # Return True if the specific update is found
                 return True
+
+    # Return False if the update is not found or if the request was not successful
     return False
--- services/downloader.py
+++ services/downloader.py
@@ -7,52 +7,61 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
+# Function to get the download directory path from the user
 def get_download_directory():
     print("Please enter the download directory path.")
     directory = input("Download Directory: ").strip()
     return directory
 
-
+# Function to validate the existence of the directory
 def validate_directory(directory):
+    # Check if the directory does not exist
     if not os.path.exists(directory):
+        # Ask the user if they want to create the directory
         create = input(f"The directory '{directory}' does not exist. Create it? (y/n): ").lower()
         if create == 'y':
+            # Create the directory
             os.makedirs(directory, exist_ok=True)
             print(f"Directory '{directory}' created.")
         else:
+            # Exit the program if the user chooses not to create the directory
             print("Exiting program.")
             exit()
+    # Check if the provided path is not a directory
     elif not os.path.isdir(directory):
         print(f"The path '{directory}' is not a directory.")
         exit()
     return directory
 
-
+# Function to initiate the download process
 def initiate_download(download_url, download_directory):
+    # Set options for the Chrome driver
     options = Options()
-    options.headless = True
+    options.headless = True  # Run Chrome in headless mode (no GUI)
     options.add_experimental_option("prefs", {
-        "download.default_directory": download_directory,
-        "download.prompt_for_download": False,
+        "download.default_directory": download_directory,  # Set the default download directory
+        "download.prompt_for_download": False,  # Disable download prompt
     })
 
+    # Set up the Chrome driver
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
-    driver.get(download_url)
+    driver.get(download_url)  # Navigate to the download URL
 
-    # Replace the below line with the actual method to find the download button or link
+    # Wait for the download button to be clickable and then click it
     wait = WebDriverWait(driver, 10)
     download_button = wait.until(EC.element_to_be_clickable((By.ID, "js-download-hero")))
     download_button.click()
 
-    driver.quit()
+    driver.quit()  # Close the browser
 
-
+# Get the download directory from the user
 download_directory = get_download_directory()
+# Validate the download directory
 download_directory = validate_directory(download_directory)
 
 # Example download URL (replace with the actual URL)
 download_url = 'https://www.google.com/chrome/'
 
+# Initiate the download process
 initiate_download(download_url, download_directory)
