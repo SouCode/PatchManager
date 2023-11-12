1c220d7563393e2f651dfb9a14f029a3c2a80318
Sou <samelsonjohn@protonmail.com>
2023-11-11 21:50:30 -0500

    Check for Updates & Initiate Downloads & Update Software List

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 from models.software import Software
+from controllers.blogMonitor import find_new_update
+from services.downloader import initiate_download, get_download_directory, validate_directory
 
 
 def load_software_list():
@@ -10,8 +12,16 @@ def load_software_list():
 
 def main():
     software_list = load_software_list()
+    download_directory = get_download_directory()
+    download_directory = validate_directory(download_directory)
+
     for software in software_list:
-        print(software)
+        print(f"Checking for updates for {software.name}...")
+        if software.name == "Google Chrome" and find_new_update():
+            print(f"New update found for {software.name}. Initiating download...")
+            initiate_download(software.download_url, download_directory)
+        else:
+            print(f"No new updates for {software.name}.")
 
 
 if __name__ == "__main__":
