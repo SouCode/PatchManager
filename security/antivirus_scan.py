2acb229915ba57f569a28e8f463270794f65f815
Sou <samelsonjohn@protonmail.com>
2023-11-19 10:47:44 -0500

    error handeling for Antivirus

--- security/antivirus_scan.py
+++ security/antivirus_scan.py
@@ -1,10 +1,11 @@
 import subprocess
+import logging
 
 
 def scan_file_with_clamav(file_path):
     try:
         # Run clamscan on the specified file
-        result = subprocess.run(['clamscan', file_path], stdout=subprocess.PIPE, text=True)
+        result = subprocess.run(['clamscan', file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         output = result.stdout
 
         # Check the output for the scan result
@@ -13,7 +14,17 @@ def scan_file_with_clamav(file_path):
         elif "FOUND" in output:
             return False  # File is infected
         else:
+            logging.warning(f"Scan inconclusive or error occurred. Output: {output}")
             return None  # Scan was inconclusive or an error occurred
+    except subprocess.CalledProcessError as e:
+        # Handle errors related to the subprocess call
+        logging.error(f"Subprocess error during antivirus scan: {e}")
+        return None
+    except FileNotFoundError:
+        # Handle the case where clamscan is not installed
+        logging.error("ClamAV is not installed or clamscan is not in the PATH.")
+        return None
     except Exception as e:
-        print(f"An error occurred during antivirus scan: {e}")
+        # Handle any other exceptions
+        logging.error(f"An unexpected error occurred during antivirus scan: {e}")
         return None
