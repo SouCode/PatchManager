7d7b189206f9ecf4860f722b249e355f96dfae50
SouCode <samelsonjohn@protonmail.com>
2023-11-30 02:53:06 -0500

    Fixed chrome search and Adobe

--- controllers/blogMonitor.py
+++ controllers/blogMonitor.py
@@ -35,109 +35,101 @@ find_firefox_update()
 
 
 def find_adobe_reader_update():
-    # Define the URL for Adobe Acrobat Reader DC release notes
     adobe_reader_blog_url = 'https://www.adobe.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/index.html'
 
     try:
-        # Attempt to send a GET request to the Adobe Reader release notes URL
         response = requests.get(adobe_reader_blog_url)
-
-        # Check if the response status code is 200 (OK)
         if response.status_code == 200:
-            # Parse the HTML content of the response
             soup = BeautifulSoup(response.text, 'html.parser')
-
-            # Find the first <li> element in the <ul class="simple"> list
             first_li = soup.select_one('ul.simple li')
             if first_li:
-                # Extract the text from the first <li> element
                 first_li_text = first_li.get_text(strip=True)
-
-                # You can add logic here to determine if the text indicates a new update
-                # For example, checking if the date in the text is more recent than a certain date
-                # For simplicity, let's assume any found text indicates a new update
-                return True
+                print(f"Latest Adobe Reader update: {first_li_text}")
         else:
-            # Print an error message if the status code is not 200
             print(f"Unexpected status code: {response.status_code}")
     except RequestException as e:
-        # Print an error message if a network error occurs
         print(f"Request failed: {e}")
     except Exception as e:
-        # Print an error message if a parsing error occurs
         print(f"An error occurred during parsing: {e}")
 
-    # Return False if no new update is found or if an error occurred
-    return False
+
+find_adobe_reader_update()
 
 
 def find_zoom_update():
-    # URL of the Zoom support page for release notes
     zoom_blog_url = 'https://support.zoom.com/hc/en/category?id=kb_category&kb_category=f55a321e8720391089a37408dabb35fa'
 
     try:
-        # Attempt to send a GET request to the Zoom support page
         response = requests.get(zoom_blog_url)
-
-        # Check if the response status code is 200 (OK)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
 
-            # Find the list of articles
-            articles_list = soup.find('ul', class_='parent-category-articles article-list-ul')
-            if articles_list:
-                # Iterate through the articles to find the newest one
-                for article in articles_list.find_all('li', {'ng-repeat': 'article in data.thisCategoriesArticles'}):
-                    article_link = article.find('a')
-                    if article_link:
-                        # Extract the date from the article title
-                        article_title = article_link.text.strip()
-                        if "Release notes for" in article_title:
-                            # Extract the date from the title and convert it to a datetime object
-                            release_date_str = article_title.replace("Release notes for ", "")
-                            release_date = datetime.strptime(release_date_str, "%B %d, %Y")
-
-                            # Compare the release date with the current date
-                            if release_date.date() > datetime.now().date():
-                                return True
+            # Adjust the selector to target the specific <ul> within the nested structure
+            target_div = soup.find('div', id='x883eb8f887c5751089a37408dabb35bc')
+            if target_div:
+                ul_element = target_div.find('ul', class_='parent-category-articles article-list-ul')
+                if ul_element:
+                    first_li = ul_element.find('li', class_='ng-scope')
+                    if first_li:
+                        article_link = first_li.find('a', class_='ng-binding')
+                        if article_link and "Release notes for" in article_link.text:
+                            release_info = article_link.text.strip()
+                            print(f"New Zoom update found: {release_info}")
+                            return
+            else:
+                print("Target div not found.")
         else:
-            # Print an error message if the status code is not 200
             print(f"Unexpected status code: {response.status_code}")
     except RequestException as e:
-        # Print an error message if a network error occurs
         print(f"Request failed: {e}")
     except Exception as e:
-        # Print an error message if a parsing error occurs
         print(f"An error occurred during parsing: {e}")
 
-    # Return False if no new update is found or if an error occurred
-    return False
+    print("No new Zoom updates found.")
+
+# Call the function
+find_zoom_update()
 
 
-def find_chrome_update():
-    chrome_blog_url = 'https://chromereleases.googleblog.com/'
+def find_and_extract_chrome_update():
+    chrome_blog_url = 'https://chromereleases.googleblog.com/search?max-results=20'
     chrome_update_text = "Beta Channel Release for ChromeOS / ChromeOS Flex"
 
     try:
-        response = requests.get(chrome_blog_url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
+        while True:
+            response = requests.get(chrome_blog_url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
 
-            for post in soup.find_all('div', class_='post'):
-                title = post.find('h2', class_='title')
-                if title and chrome_update_text in title.get_text():
-                    date_tag = post.find('span', class_='publishdate')
-                    date_text = date_tag.get_text(strip=True) if date_tag else "Unknown Date"
-                    return True, date_text, post
-        else:
-            print(f"Unexpected status code: {response.status_code}")
+                for post in soup.find_all('div', class_='post'):
+                    title = post.find('h2', class_='title')
+                    if title and chrome_update_text in title.get_text():
+                        date_text = post.find('span', class_='publishdate').get_text(strip=True)
+                        update_info = extract_update_info_from_post(post)
+                        if update_info:
+                            print(f"Update found on {date_text}: {update_info}")
+                            return
+                        else:
+                            print("Update information not found.")
+                            return
+
+                # Check for a link to the next page
+                older_link = soup.find('a', string='Older Posts')
+                if older_link and 'href' in older_link.attrs:
+                    chrome_blog_url = older_link['href']  # Update the URL for the next page
+                else:
+                    break  # Exit the loop if there are no more pages
+            else:
+                print(f"Unexpected status code: {response.status_code}")
+                break
+
     except RequestException as e:
         print(f"Request failed: {e}")
 
-    return False, None, None
+    print("No update found.")
 
 
-def extract_beta_update_info(post):
+def extract_update_info_from_post(post):
     search_phrase = "The Beta channel has been updated to"
     post_body_div = post.find('div', class_='post-body')
     if post_body_div:
@@ -145,22 +137,13 @@ def extract_beta_update_info(post):
         for p in paragraphs:
             if search_phrase in p.get_text():
                 update_info = p.get_text(strip=True)
-                # Correcting the spacing issue
                 update_info = update_info.replace("updated to", "updated to ")
-
-                # Removing the platform version part
                 platform_version_index = update_info.find("(Platform version:")
                 if platform_version_index != -1:
                     update_info = update_info[:platform_version_index].strip() + " for ChromeOS devices."
-
                 return update_info
-    return "Update information not found"
+    return None
 
 
-update_found, update_date, post = find_chrome_update()
-if update_found:
-    print(f"Update found on {update_date}: ")
-    update_info = extract_beta_update_info(post)
-    print(update_info)
-else:
-    print("No update found.")
+# Call the function
+find_and_extract_chrome_update()
